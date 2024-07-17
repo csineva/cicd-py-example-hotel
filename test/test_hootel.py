55c69d87fc21fa4f763bada3cff7cde2a0ec5174
Benedek István <47147992+csineva@users.noreply.github.com>
2024-07-17 19:18:17 +0200

    Update test_hootel.py

--- test/test_hootel.py
+++ test/test_hootel.py
@@ -14,7 +14,7 @@ class TestHootel(object):
         options.add_argument('--headless')
         self.browser = webdriver.Chrome(options=options)
         self.browser.get(URL)
-        self.browser.maximize_window()
+        self.browser.set_window_size(1024, 800)
 
     def teardown_method(self):
         self.browser.quit()
@@ -24,8 +24,8 @@ class TestHootel(object):
     @allure.severity(allure.severity_level.TRIVIAL)
     @allure.tag("login")
     def test_login(self):
-        menu_toggle = self.browser.find_element(By.XPATH, '//span[@class="navbar-toggler-icon"]')
-        menu_toggle.click()
+        # menu_toggle = self.browser.find_element(By.XPATH, '//span[@class="navbar-toggler-icon"]')
+        # menu_toggle.click()
         login_btn = self.browser.find_element(By.XPATH, '//a[@class="nav-link"]')
         login_btn.click()
 
